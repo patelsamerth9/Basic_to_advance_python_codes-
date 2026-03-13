95a78854a6c7fe80288a5b47313bc132ebbc35d2
patelsamerth9 <patelsamerth9@gmail.com>
2026-03-13 20:22:28 +0530

    Loan Risk using python comands

--- Loan RiskPrediction.py
+++ Loan RiskPrediction.py
@@ -17,7 +17,8 @@ model = LogisticRegression()
 model.fit(X, y)
 
 new_customer = [[30000, 20000, 600]]
-
+# The new customer's income is $30,000, loan amount is $20,000, and credit score is 600.
 prediction = model.predict(new_customer)
 
 print("Risk Prediction:", prediction)
+# The output will be either 0 (low risk) or 1 (high risk) based on the model's prediction for the new customer.
