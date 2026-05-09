29f2f826d75892c434b941c8580c1fc6369938c3
patelsamerth9 <patelsamerth9@gmail.com>
2026-05-10 00:02:03 +0530

    Add new implementations for Joining & Splitting Strings, Library Management System, Linear Search, Loan Risk Prediction, Loan EMI Calculator, Logic Building, and Login Page for enhanced functionality and demonstration of concepts

--- Joining&SplittingStrings.py
+++ Joining&SplittingStrings.py
@@ -1,6 +1,5 @@
 words = ["Python", "is", "fun"]
 sentence = " ".join(words)
 print(sentence)
-
 split_words = sentence.split()
 print(split_words)
--- LibraryManagementSystem.py
+++ LibraryManagementSystem.py
@@ -1,38 +1,27 @@
-# Simple Library Management System
-
 library = []
-
-def add_book():# Function to add a book to the library
-    book_id = input("Enter Book ID: ")# Get book details from user
+def add_book():
+    book_id = input("Enter Book ID: ")
     book_name = input("Enter Book Name: ")
     author = input("Enter Author Name: ")
-
     book = {
         "id": book_id,
         "name": book_name,
         "author": author,
         "issued": False
     }
-
     library.append(book)
     print("Book added successfully!\n")
-
-
 def show_books():
     if not library:
         print("No books available\n")
         return
-
     print("\n📚 Library Books:")
     for book in library:
         status = "Issued" if book["issued"] else "Available"
         print(f'ID: {book["id"]}, Name: {book["name"]}, Author: {book["author"]}, Status: {status}')
     print()
-
-
 def issue_book():# Function to issue a book
     book_id = input("Enter Book ID to issue: ")
-
     for book in library:
         if book["id"] == book_id:
             if not book["issued"]:
@@ -42,13 +31,9 @@ def issue_book():# Function to issue a book
             else:
                 print(" Book already issued\n")
                 return
-
     print("Book not found\n")
-
-
 def return_book():
     book_id = input("Enter Book ID to return: ")
-
     for book in library:
         if book["id"] == book_id:
             if book["issued"]:
@@ -58,10 +43,7 @@ def return_book():
             else:
                 print("Book was not issued\n")
                 return
-
     print(" Book not found\n")
-
-
 while True:
     print("====== Library Management System ======")
     print("1. Add Book")
@@ -69,9 +51,7 @@ while True:
     print("3. Issue Book")
     print("4. Return Book")
     print("5. Exit")
-
     choice = input("Enter your choice: ")
-
     if choice == "1":
         add_book()
     elif choice == "2":
